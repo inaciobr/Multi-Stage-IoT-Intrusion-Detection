b4e57ce5cf55db5c83c774a991897ebc22dbc7c8
Bruno <brunob.inacio@gmail.com>
2024-04-14 13:08:56 -0300

    Add colors to the EDA

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ import yaml
 
 
 def get_constants():
-    with open(os.path.join(os.getcwd(), '..', 'constants.yaml' )) as f:
+    with open(os.path.join(os.getcwd(), '..', 'constants.yaml')) as f:
         constants = yaml.safe_load(f)
 
     constants['parquet_path'] = os.path.join(constants['path'], constants['parquet_name'])
@@ -17,6 +17,12 @@ def get_constants():
         col: attack_category
         for attack_category, column_list in constants['attack_category'].items()
         for col in column_list
+    }
+
+    constants['protocol_layer_map'] = {
+        protocol: layer
+        for layer, protocol_list in constants['protocol_layer'].items()
+        for protocol in protocol_list
     }
 
     return constants
