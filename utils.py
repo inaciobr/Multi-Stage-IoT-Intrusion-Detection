3690e744d02696e87b51e285bd12d7cf706e5716
Bruno <brunob.inacio@gmail.com>
2024-05-09 16:40:42 -0300

    Add weighted sampling function

--- utils.py
+++ utils.py
@@ -28,3 +28,18 @@ def get_constants():
     }
 
     return constants
+
+
+def get_balanced_weights(df, target):
+    counts = df[target].value_counts()
+    num_labels = len(counts)
+
+    return 1. / df[target].map(counts).astype('int') / num_labels
+
+
+def get_features_list(df, constants):
+    return [
+        col
+        for col in df.columns
+        if col not in constants['target_columns']
+    ]
